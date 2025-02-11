2b6fb88bb6c6b5fce3f8624aeeac1e30b645d94e
Piyush Channawar <channawarpiyush@gmail.com>
2025-02-12 00:59:53 +0530

    final for diployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import productRoutes from './routes/productRoutes.js';
 import { sql } from './config/db.js';
@@ -14,10 +15,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet(
+    {
+        contentSecurityPolicy: false,
+    }
+));
 app.use(morgan('dev'));
 
 // apply arcjet rate-limit to all routes
@@ -53,6 +59,14 @@ app.use(async (req, res, next) => {
 
 app.use("/api/products", productRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+  // server our react app
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  })
+}
 
 async function initDB() {
     try {
